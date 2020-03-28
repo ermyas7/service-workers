c782755da5cd40856be8fa8ab94a9bef72108aa5
Ermyas Fekadu <ermyas.fekadu@gmail.com>
2020-03-28 20:54:29 +0530

    Register service worker

--- web/js/blog.js
+++ web/js/blog.js
@@ -6,12 +6,22 @@
 
 	var isOnline = ("onLine" in navigator)? navigator.onLine : true;
 
+	//service worker feature detection
+
+	var usingSw = ("serviceWorker" in navigator);
+	var swRegistration;
+	var svcWorker;
+
+	//register and install service worker
+	initServiceWorker().catch(console.error);
+
 	document.addEventListener("DOMContentLoaded",ready,false);
 
 
 	// **********************************
 
 	function ready() {
+		//UI for offline and online case
 		offlineIcon = document.getElementById("connectivity-status");
 
 		if(!isOnline){
@@ -26,6 +36,22 @@
 		window.addEventListener("offline", function offline(){
 			offlineIcon.classList.remove("hidden");
 			isOnline = false;
+		});
+	}
+
+	async function initServiceWorker(){
+
+		swRegistration = await navigator.serviceWorker.register("/sw.js", {
+			updateViaCache: "none"
+		});
+
+		//service worker access
+
+		svcWorker = swRegistration.installing || swRegistration.waiting || swRegistration.active;
+
+		//update the data
+		navigator.serviceWorker.addEventListener("controllerchange", function onControll(){
+			svcWorker = navigator.serviceWorker.controller;
 		});
 	}
 
--- web/js/sw.js
+++ web/js/sw.js
@@ -1,3 +1,35 @@
 "use strict";
 
 // TODO
+
+const version = 1;
+
+//install sw
+self.addEventListener("install", onInstall);
+
+//activate sw
+self.addEventListener("activate", onActivate);
+
+main().catch(console.error);
+
+
+//************************************************************* */
+async function main(){
+    console.log(`Service worker version-${version} is starting...`);
+}
+
+async function onInstall(evt){
+    console.log(`Service worker version-${version} is installed.`);
+    self.skipWaiting();
+}
+
+async function onActivate(evt){
+    evt.waitUntil(handleActivation());
+}
+
+async function handleActivation(){
+    //update all tabs
+    await clients.claim(); 
+    console.log(`Service worker version-${version} is activated.`);
+}
+
